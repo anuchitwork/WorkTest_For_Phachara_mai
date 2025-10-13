d39464d3a61ec9b26374df455001a7662eb80e5b
anuchit22 <anuchitwork22@gmail.com>
2025-10-13 15:31:15 +0700

    add New  TypeOrmModule

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,14 +1,33 @@
 import { DataSource } from 'typeorm';
 import { User } from './users/entities/user.entity';
 
-export const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource(
+  
+  
+//   {
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root',
+//   password: '',
+//   database: 'worktest_for_phachara_mai',
+//   entities: [User],
+//   migrations: ['src/migrations/*.ts'], // folder migration
+//   synchronize: false,
+// }
+
+
+  {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'worktest_for_phachara_mai',
-  entities: [User],
-  migrations: ['src/migrations/*.ts'], // folder migration
-  synchronize: false,
-});
+  host: 'mysql-nunu.alwaysdata.net',      // <-- เปลี่ยนจาก localhost
+  port: 3306,                              // Default ของ MySQL
+  username: 'nunu',                        // <-- user ที่สร้างใน AlwaysData
+  password: 'Anuanuanu22',               // <-- ใส่ password ที่ตั้งไว้
+  database: 'nunu_worktest_for_phachara_mai', // <-- ชื่อ database ที่สร้าง
+  entities: [User],                        // entity ของ NestJS
+  synchronize: true,                        // ใช้ true ตอน dev เพื่อสร้าง table อัตโนมัติ
+}
+
+
+
+);
